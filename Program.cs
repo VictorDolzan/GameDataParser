8c368ee4e3042d12d36a5e51ee5784c2437dbe46
Victor Dolzan <victorddf5@gmail.com>
2024-02-17 10:37:35 -0300

    📜Implemented the read and deserialize from a json file using VideoGame class

--- Program.cs
+++ Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text.Json;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter the name of the file you want to read: ");
+var fileName = Console.ReadLine();
+
+var fileContents = File.ReadAllText(fileName);
+
+var videoGames = JsonSerializer.Deserialize<List<VideoGame>>(fileContents);
+
+if (videoGames.Count > 0)
+{
+    Console.WriteLine();
+    Console.WriteLine("Loaded games are: ");
+    foreach (var videoGame in videoGames)
+    {
+        Console.WriteLine(videoGame);
+    }
+}
+else
+{
+    Console.WriteLine("No games are present in the input file"); 
+}
+
+Console.ReadKey();
+
+
+public class VideoGame
+{
+    public string Title { get; init; }
+    public int ReleaseYear { get; init; }
+    public decimal Rating { get; init; }
+
+    public override string ToString() => @$"{Title}, released in {ReleaseYear}, Rating: {Rating}";
+}
